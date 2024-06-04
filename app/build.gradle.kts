be639f6847ac8e27072bc0a7037e30cdbfa22bea
MD Mahabubur Rohman Meraj <mahabubmaraj@gmail.com>
2024-06-04 13:11:57 +0600

    4th Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -48,4 +49,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+        val room_version = "2.6.1"
+
+        implementation("androidx.room:room-runtime:$room_version")
+        annotationProcessor("androidx.room:room-compiler:$room_version")
+
+        // To use Kotlin Symbol Processing (KSP)
+        ksp("androidx.room:room-compiler:$room_version")
+
+        // optional - Kotlin Extensions and Coroutines support for Room
+        implementation("androidx.room:room-ktx:$room_version")
 }
--- app/src/main/java/com/example/recycleview/Data.kt
+++ app/src/main/java/com/example/recycleview/Data.kt
@@ -1,6 +1,12 @@
 package com.example.recycleview
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "data_table")
 data class Data(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val name: String,
     val detail: String,
     val image: Int,
