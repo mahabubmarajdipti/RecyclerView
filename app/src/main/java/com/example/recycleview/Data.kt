26cea1fbcf5b1007888e228a5499be7aeab2c591
MD Mahabubur Rohman Meraj <mahabubmaraj@gmail.com>
2024-06-06 12:10:21 +0600

    from dipti pc

--- app/src/main/java/com/example/recycleview/Data.kt
+++ app/src/main/java/com/example/recycleview/Data.kt
@@ -1,5 +1,6 @@
 package com.example.recycleview
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -7,7 +8,10 @@ import androidx.room.PrimaryKey
 data class Data(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo(name = "data_name")
     val name: String,
+    @ColumnInfo(name = "data_detail")
     val detail: String,
+    @ColumnInfo(name = "data_image")
     val image: Int,
 )
